a04decec305c458da77b184884cca52909b7f2ed
Cam <cameron28202@gmail.com>
2025-03-28 22:20:08 -0500

    start order functionality

--- BubbleFlowBackend/src/main/java/com/team64/BubbleFlowBackend/model/OrderItem.java
+++ BubbleFlowBackend/src/main/java/com/team64/BubbleFlowBackend/model/OrderItem.java
@@ -1,11 +1,57 @@
 package com.team64.BubbleFlowBackend.model;
 
 import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "Order_Items")
+@Table(name = "order_items")
 public class OrderItem {
 
     @Id
+    private int order_item_id;
+
+    @Column(nullable = false)
+    private int drink_id;
+
+    @Column(name = "id", nullable = false)
     private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    @JsonIgnore
+    private Order order;
+
+    public OrderItem() {}
+
+    public int getOrder_item_id() {
+        return order_item_id;
+    }
+
+    public void setOrder_item_id(int order_item_id) {
+        this.order_item_id = order_item_id;
+    }
+
+    public int getDrink_id() {
+        return drink_id;
+    }
+
+    public void setDrink_id(int drink_id) {
+        this.drink_id = drink_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
---
+++ BubbleFlowBackend/src/main/java/com/team64/BubbleFlowBackend/repository/OrderRepo.java
@@ -0,0 +1,7 @@
+package com.team64.BubbleFlowBackend.repository;
+
+import com.team64.BubbleFlowBackend.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepo extends JpaRepository<Order, Integer> {
+}
