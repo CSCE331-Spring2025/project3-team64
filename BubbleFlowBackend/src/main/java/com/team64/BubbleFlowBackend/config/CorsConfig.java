5205765b4471f4ecdbfa22b5e2a679080bfc7156
Cam <cameron28202@gmail.com>
2025-04-02 09:43:05 -0500

    cors urls

--- BubbleFlowBackend/src/main/java/com/team64/BubbleFlowBackend/config/CorsConfig.java
+++ BubbleFlowBackend/src/main/java/com/team64/BubbleFlowBackend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://bubble_flow.vercel.app/")
+                        .allowedOrigins("http://localhost:3000", "https://bubbleflow.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
