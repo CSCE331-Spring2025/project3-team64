abfe8dc038e0ba9fc18e62d9cfba417beaee85e1
Sophia Phu <sophiatiffphu@gmail.com>
2025-04-04 13:19:09 -0500

    started menu editing page

---
+++ bubbleflow-frontend/src/app/edit-menu/page.tsx
@@ -0,0 +1,165 @@
+"use client";
+import { useState, useEffect, ChangeEvent } from "react";
+import { Input } from "@/components/ui/input";
+import MenuCategory from "@/components/menuCategory";
+import { RiSearchLine, RiAddLine } from "react-icons/ri";
+import DrinkEditCard from "@/components/drinkEditCard";
+import { useDrinks, useDrinkCategories } from "../hooks/useDrinks";
+
+export default function EditMenu() {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const {
+    drinks,
+    loading: drinksLoading,
+    error: drinksError,
+    fetchDrinks,
+  } = useDrinks();
+
+  const {
+    drinkCategories: categories,
+    loading: categoriesLoading,
+    error: categoriesError,
+    fetchDrinkCategories,
+  } = useDrinkCategories();
+
+  useEffect(() => {
+    fetchDrinks();
+    fetchDrinkCategories();
+  }, [fetchDrinks, fetchDrinkCategories]);
+
+  if (drinksLoading || categoriesLoading) {
+    return <div className="px-16">Loading...</div>;
+  }
+
+  if (drinksError || categoriesError) {
+    return (
+      <div className="px-16">
+        Error: {drinksError || categoriesError}
+      </div>
+    );
+  }
+
+  const imageMap: Record<string, string> = {
+    "Classic Pearl Milk Tea": "/classic-pearl-milk-tea.png",
+    "Honey Milk Tea": "/honey-milk-tea.png",
+    "Classic Coffee": "/classic-coffee.png",
+    "Ginger Milk Tea": "/ginger-milk-tea.png",
+    "Thai Pearl Milk Tea": "/thai-pearl-milk-tea.png",
+    "Taro Pearl Milk Tea": "/taro-milk-tea.png",
+    "Classic Tea": "/classic-tea.png",
+    "Wintermelon Tea": "/wintermelon-tea.png",
+    "Honey Tea": "/honey-tea.png",
+    "Ginger Tea": "/ginger-tea.png",
+    "Mango Green Tea": "/mango-green-tea.png",
+    "Wintermelon Lemonade": "/wintermelon-tea.png",
+    "Strawberry Tea": "/strawberry-tea.png",
+    "Peach Tea with Aiyu Jelly": "/peach-tea-with-aiyu-jelly.png",
+    "Kiwi Fruit Tea with Aiyu Jelly": "/kiwi-fruit-tea-with-aiyu-jelly.png",
+    "Mango & Passion Fruit Tea": "/mango-&-passion-fruit-tea.png",
+    "Cocoa Lover with Fresh Milk": "/cocoa-lover-with-fresh-milk.png",
+    "Homemade Taro with Fresh Milk": "/homemade-taro-with-fresh-milk.png",
+    "Matcha with Fresh Milk": "/matcha-with-fresh-milk.png",
+    "Oreo Ice Blended with Pearl": "/oreo-ice-blended-with-pearl.png",
+    "Matcha Red Bean Ice Blended with Ice Cream":
+      "/matcha-red-bean-ice-blended-with-ice-cream.png",
+    "Coffee Ice Blended with Ice Cream": "/coffee-ice-blended-with-ice-cream.png",
+    "Mango Ice Blended with Ice Cream": "/mango-ice-blended-with-ice-cream.png",
+    "Strawberry Ice Blended with Lychee Jelly & Ice Cream":
+      "/strawberry-ice-blended-with-lychee-and-ice-cream.png",
+    "Lime Mojito": "/lime-mojito.png",
+    "Mango Mojito": "/mango-mojito.png",
+    "Peach Mojito": "/peach-mojito.png",
+    "Strawberry Mojito": "/strawberry-mojito.png",
+    "Creama Tea": "/creama-tea.png",
+    "Match Creama": "/matcha-creama.png",
+    "Coffee Creama": "/coffee-creama.png",
+    "Cocoa Creama": "/cocoa-creama.png",
+  };
+
+  const filteredDrinks = drinks.filter((drink) => {
+    const matchesCategory = selectedCategory
+      ? drink?.drink_category_id?.drink_category_name === selectedCategory
+      : true;
+    const matchesSearch = drink?.drink_name
+      ?.toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    return matchesCategory && matchesSearch;
+  });
+
+  const orderedDrinks = [...filteredDrinks].sort((a, b) => {
+    const aCat = a.drink_category_id?.drink_category_name || "";
+    const bCat = b.drink_category_id?.drink_category_name || "";
+    return aCat.localeCompare(bCat);
+  });
+
+  return (
+    <main className="flex flex-col px-16 pb-8">
+      <div>
+        <div className="flex flex-wrap gap-2">
+          <div
+            key="all-drinks"
+            onClick={() => setSelectedCategory(null)}
+            className="cursor-pointer"
+          >
+            <MenuCategory categoryName="All Drinks" itemCount={drinks.length} />
+          </div>
+          {categories.map((category, index) => {
+            const categoryName = category.drink_category_name || "No Category";
+            const count = drinks.filter(
+              (drink) =>
+                drink?.drink_category_id?.drink_category_name === categoryName
+            ).length;
+            return (
+              <div
+                key={index}
+                onClick={() => setSelectedCategory(categoryName)}
+                className="cursor-pointer"
+              >
+                <MenuCategory categoryName={categoryName} itemCount={count} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="mt-2 flex items-center gap-2">
+        <div className="relative w-full">
+          <Input
+            className="border-[#6F403A] h-10 rounded-3xl pr-12"
+            placeholder="Search for Menu Item"
+            value={searchTerm}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setSearchTerm(e.target.value)
+            }
+          />
+          <div className="absolute right-3 top-1/2 transform -translate-y-1/2 bg-[#6F403A] w-7 h-7 rounded-full flex items-center justify-center">
+            <RiSearchLine className="text-white" size={15} />
+          </div>
+        </div>
+        <div>
+          <div className="bg-[#6F403A] w-8 h-8 rounded-full flex items-center justify-center">
+            <RiAddLine className="text-white" size={18} />
+          </div>
+        </div>
+      </div>
+      <div className="mt-2 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-2">
+        {orderedDrinks.map((drink, index) => (
+          <DrinkEditCard
+            key={index}
+            drinkName={drink?.drink_name || "No Name"}
+            drinkCategory={
+              drink?.drink_category_id?.drink_category_name || "No Category"
+            }
+            drinkPrice={drink?.drink_price || 0}
+            imageSrc={
+              imageMap[drink.drink_name] || "/classic-pearl-milk-tea.png"
+            }
+            drinkId={drink?.drink_id}
+            itemId={Date.now()}
+          />
+        ))}
+      </div>
+    </main>
+  );
+}
---
+++ bubbleflow-frontend/src/components/drinkEditCard.tsx
@@ -0,0 +1,316 @@
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { useState, useEffect } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Image from "next/image";
+import { useExtras } from "@/app/hooks/useExtras";
+import { Extra } from "@/app/service/types";
+
+interface DrinkCardProps {
+  drinkName: string;
+  drinkCategory: string | undefined;
+  drinkPrice: number;
+  imageSrc: string;
+  drinkId: number;
+  itemId: number;
+}
+
+export default function DrinkEditCard({
+  drinkName,
+  drinkCategory,
+  drinkPrice,
+  imageSrc,
+  drinkId,
+  itemId,
+}: DrinkCardProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const categoryColors: Record<string, { badgeBg: string; badgeText: string }> =
+    {
+      "Milk Teas": { badgeBg: "bg-[#ead2a2]", badgeText: "text-[#6F403A]" },
+      "Brewed Tea": { badgeBg: "bg-[#dfcebb]", badgeText: "text-[#6F403A]" },
+      "Fruit Tea": { badgeBg: "bg-[#dbb9a7]", badgeText: "text-[#6F403A]" },
+      "Fresh Milk": { badgeBg: "bg-[#f0dece]", badgeText: "text-[#6F403A]" },
+      "Ice Blended": { badgeBg: "bg-[#ebd1b5]", badgeText: "text-[#6F403A]" },
+      "Tea Mojito": { badgeBg: "bg-[#f6cdb1]", badgeText: "text-[#6F403A]" },
+      Creama: { badgeBg: "bg-[#f3ecdf]", badgeText: "text-[#6F403A]" },
+    };
+
+  const categoryColor = (drinkCategory && categoryColors[drinkCategory]) || {
+    badgeBg: "bg-[#f0dece]",
+    badgeText: "text-[#6F403A]",
+  };
+  return (
+    <div className="border border-[#6F403A] p-2 rounded-xl flex flex-col justify-between">
+      <div
+        className={`${categoryColor.badgeBg} rounded-xl flex justify-center py-4`}
+      >
+        <div className="transition-transform duration-300 hover:scale-110">
+          <Image src={imageSrc} alt={drinkName} width={75} height={75} />
+        </div>
+      </div>
+      <p className="mt-2 font-semibold">{drinkName}</p>
+      <div className="flex justify-between mt-1">
+        <Badge
+          className={`${categoryColor.badgeBg} ${categoryColor.badgeText} font-normal px-3 rounded-3xl`}
+        >
+          {drinkCategory}
+        </Badge>
+        <p className="text-sm">${Number(drinkPrice).toFixed(2)}</p>
+      </div>
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogTrigger asChild>
+          <Button className="mt-6 w-full bg-[#6F403A] hover:bg-[#4E2D26]">
+            Edit Drink
+          </Button>
+        </DialogTrigger>
+        {isOpen && (
+          <DrinkCustomizationDialog
+            drinkName={drinkName}
+            drinkCategory={drinkCategory}
+            drinkPrice={drinkPrice}
+            imageSrc={imageSrc}
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            drinkId={drinkId}
+            itemId={itemId}
+          />
+        )}
+      </Dialog>
+    </div>
+  );
+}
+
+// Separate component for the dialog contents
+function DrinkCustomizationDialog({
+  drinkName,
+  drinkCategory,
+  drinkPrice,
+  imageSrc,
+  isOpen,
+  drinkId,
+  setIsOpen,
+}: DrinkCardProps & { isOpen: boolean; setIsOpen: (open: boolean) => void }) {
+  const { extras, loading: extrasLoading, fetchExtras } = useExtras();
+
+  // Fetch extras when the dialog opens
+  useEffect(() => {
+    if (isOpen) {
+      fetchExtras();
+    }
+  }, [isOpen, fetchExtras]);
+
+  const filterExtraByCategoryId = (
+    extras: Extra[],
+    categoryId: number
+  ): Extra[] => {
+    return extras.filter(
+      (extra) => extra.extra_category_id.extra_category_id === categoryId
+    );
+  };
+  const sugarOptions: Extra[] = filterExtraByCategoryId(extras || [], 2);
+  const iceOptions: Extra[] = filterExtraByCategoryId(extras || [], 1);
+  const toppings: Extra[] = filterExtraByCategoryId(extras || [], 3);
+
+  // store the full Extra objects
+  const [selectedSugarObj, setSelectedSugarObj] = useState<Extra | null>(null);
+  const [selectedIceObj, setSelectedIceObj] = useState<Extra | null>(null);
+  const [selectedToppings, setSelectedToppings] = useState<Extra[]>([]);
+
+  // initialize defaults when extras data is available
+  useEffect(() => {
+    if (sugarOptions.length > 0 && !selectedSugarObj) {
+      setSelectedSugarObj(
+        sugarOptions.length > 1 ? sugarOptions[1] : sugarOptions[0]
+      );
+    }
+    if (iceOptions.length > 0 && !selectedIceObj) {
+      setSelectedIceObj(iceOptions[0]);
+    }
+  }, [sugarOptions, iceOptions, selectedSugarObj, selectedIceObj]);
+
+  // handle sugar selection
+  const handleSugarChange = (value: string) => {
+    const sugar = sugarOptions.find((option) => option.extra_name === value);
+    if (sugar) {
+      setSelectedSugarObj(sugar);
+    }
+  };
+
+  // handle ice selection
+  const handleIceChange = (value: string) => {
+    const ice = iceOptions.find((option) => option.extra_name === value);
+    if (ice) {
+      setSelectedIceObj(ice);
+    }
+  };
+
+  // handle topping selection
+  const handleToppingSelection = (topping: Extra) => {
+    setSelectedToppings((prev) => {
+      const exists = prev.some((t) => t.extra_id === topping.extra_id);
+      if (exists) {
+        return prev.filter((t) => t.extra_id !== topping.extra_id);
+      } else {
+        return [...prev, topping];
+      }
+    });
+  };
+
+  const handleAddToOrder = () => {
+    const orderItem = {
+      drinkName,
+      drinkCategory,
+      drinkPrice,
+      topPrice: 0,
+      totalPrice: 0,
+      imageSrc,
+      sugarLevel: selectedSugarObj?.extra_name || "No Sugar",
+      iceLevel: selectedIceObj?.extra_name || "No Ice",
+      toppings: selectedToppings.length === 0 ? ["No Toppings"] : selectedToppings.map(t => t.extra_name),
+      toppingIds: selectedToppings.map(t => t.extra_id),
+      drinkId,
+      itemId: Date.now(),
+    };
+
+    // Edit the price total Local Variable
+    orderItem.topPrice = selectedToppings.reduce((acc, topping) => acc + topping.extra_price, 0);
+    orderItem.totalPrice = orderItem.topPrice + orderItem.drinkPrice;
+    const currentTotal = parseFloat(localStorage.getItem("orderprice") || "0");
+    const newTotal = currentTotal + orderItem.totalPrice;
+    localStorage.setItem("orderprice", newTotal.toString());
+
+    //Save the total drink price as the price of the drink + toppings (this is 7 billion times easier than the alternative)
+    const existingOrders = JSON.parse(localStorage.getItem("orderItems") || "[]");
+    existingOrders.push(orderItem);
+    localStorage.setItem("orderItems", JSON.stringify(existingOrders));
+
+    setIsOpen(false);
+  };
+
+  return (
+    <DialogContent className="sm:max-w-[425px]">
+      <DialogHeader>
+        <DialogTitle>{drinkName}</DialogTitle>
+      </DialogHeader>
+      <div className="flex flex-col gap-8 py-4">
+        <div className="items-center gap-4">
+          <Label className="mb-2">Sugar</Label>
+          <Select
+            onValueChange={handleSugarChange}
+            value={selectedSugarObj?.extra_name}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue
+                placeholder={
+                  extrasLoading ? "Loading options..." : "Select an Option"
+                }
+              />
+            </SelectTrigger>
+            <SelectContent>
+              {sugarOptions.length > 0 ? (
+                sugarOptions.map((option) => (
+                  <SelectItem key={option.extra_id} value={option.extra_name}>
+                    {option.extra_name}
+                  </SelectItem>
+                ))
+              ) : (
+                <SelectItem disabled value="loading">
+                  {extrasLoading
+                    ? "Loading options..."
+                    : "No options available"}
+                </SelectItem>
+              )}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="items-center gap-4">
+          <Label className="mb-2">Ice</Label>
+          <Select
+            onValueChange={handleIceChange}
+            value={selectedIceObj?.extra_name}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue
+                placeholder={
+                  extrasLoading ? "Loading options..." : "Select an Option"
+                }
+              />
+            </SelectTrigger>
+            <SelectContent>
+              {iceOptions.length > 0 ? (
+                iceOptions.map((option) => (
+                  <SelectItem key={option.extra_id} value={option.extra_name}>
+                    {option.extra_name}
+                  </SelectItem>
+                ))
+              ) : (
+                <SelectItem disabled value="loading">
+                  {extrasLoading
+                    ? "Loading options..."
+                    : "No options available"}
+                </SelectItem>
+              )}
+            </SelectContent>
+          </Select>
+        </div>
+        <div>
+          <Label className="mb-2">Toppings</Label>
+          <div className="flex flex-wrap gap-2">
+            {toppings.length > 0 ? (
+              toppings.map((topping) => {
+                const isSelected = selectedToppings.some(
+                  (t) => t.extra_id === topping.extra_id
+                );
+                return (
+                  <Badge
+                    key={topping.extra_id}
+                    className="rounded-4xl px-2 bg-white text-black border-gray-200 flex items-center"
+                    variant={isSelected ? "default" : "outline"}
+                    onClick={() => handleToppingSelection(topping)}
+                  >
+                    <div
+                      className={
+                        isSelected
+                          ? "w-4 h-4 rounded-full border mr-1 bg-black border-black"
+                          : "w-4 h-4 rounded-full border mr-1"
+                      }
+                    ></div>
+                    <p className="text-sm font-normal">{topping.extra_name}</p>
+                  </Badge>
+                );
+              })
+            ) : (
+              <div>
+                {extrasLoading
+                  ? "Loading toppings..."
+                  : "No toppings available"}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      <Button
+        type="submit"
+        className="bg-[#6F403A] hover:bg-[#4E2D26]"
+        onClick={handleAddToOrder}
+        disabled={extrasLoading}
+      >
+        Add Item to Order
+      </Button>
+    </DialogContent>
+  );
+}
