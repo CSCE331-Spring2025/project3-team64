c48216a70c3258f8da7f243e41a1559d44f1d510
Sophia Phu <sophiatiffphu@gmail.com>
2025-04-04 18:22:01 -0500

    created search bar (ui)

--- bubbleflow-frontend/src/app/manage-employees/page.tsx
+++ bubbleflow-frontend/src/app/manage-employees/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
 import EmployeeCard from "@/components/employeeCard";
+import { RiSearchLine, RiAddLine } from "react-icons/ri";
 
 export default function ManageEmployees() {
   const employees = [
@@ -15,16 +14,30 @@ export default function ManageEmployees() {
     },
     {
       id: "987654321",
-      name: "John Doe",
-      email: "john.doe@example.com",
+      name: "Ur Mom",
+      email: "urmomsigma69@ligma.com",
       phone: "(555) 123 4567",
       role: "Manager",
     },
   ];
   return (
     <main className="flex flex-col px-16">
-      <div className=" text-xl font-semibold">
-        <p>Employee List</p>
+      <div className="flex items-center justify-between">
+        <p className=" text-xl font-semibold">Employee List</p>
+        <div className=" flex items-center gap-2">
+          <div className="mt-2 relative w-80">
+            <Input
+              className="border-[#6F403A] h-10 rounded-3xl pr-12"
+              placeholder="Search for Menu Item"
+            />
+            <div className="absolute right-3 top-1/2 transform -translate-y-1/2 bg-[#6F403A] w-7 h-7 rounded-full flex items-center justify-center">
+              <RiSearchLine className="text-white" size={15} />
+            </div>
+          </div>
+          <div className="bg-[#6F403A] w-8 h-8 rounded-full flex items-center justify-center hover:bg-[#4E2D26] mt-2">
+            <RiAddLine className="text-white" size={18} />
+          </div>
+        </div>
       </div>
       <div className=" mt-4 flex flex-col gap-2">
         {employees.map((employee) => (
