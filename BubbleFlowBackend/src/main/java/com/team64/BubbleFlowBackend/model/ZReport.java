7d17ac820a7f7981bbc966bef93303739bfb1cf8
abhisriDS <abhisri.dasari@tamu.edu>
2025-04-08 01:04:22 -0500

    working on reports API with SQL stuff?; redo service/controller; commented for now

---
+++ BubbleFlowBackend/src/main/java/com/team64/BubbleFlowBackend/model/ZReport.java
@@ -0,0 +1,43 @@
+// package com.team64.BubbleFlowBackend.model;
+
+// import jakarta.persistence.*;
+// import java.sql.Timestamp;
+// import java.util.ArrayList;
+// import java.util.List;
+
+// public class ZReport {
+//     private String GROSS_SALES;
+//     private String TAX;
+//     private String TOTAL_NET_SALES;
+
+//     public ZReport(String GROSS_SALES, String TAX, String TOTAL_NET_SALES) {
+//         this.GROSS_SALES = GROSS_SALES; // unsure
+//         this.TAX = TAX; // unsure
+//         this.TOTAL_NET_SALES = TOTAL_NET_SALES; // unsure
+//     }
+
+//     public String getGROSS_SALES() {
+//         return GROSS_SALES;
+//     }
+
+//     public void setGROSS_SALES(String GROSS_SALES) {
+//         this.GROSS_SALES = GROSS_SALES;
+//     }
+
+//     public String getTAX() {
+//         return TAX;
+//     }
+
+//     public void setTAX(String TAX) {
+//         this.TAX = TAX;
+//     }
+
+//     public String getTOTAL_NET_SALES() {
+//         return TOTAL_NET_SALES;
+//     }
+
+//     public void setTOTAL_NET_SALES(String TOTAL_NET_SALES) {
+//         this.TOTAL_NET_SALES = TOTAL_NET_SALES;
+//     }
+
+// }
