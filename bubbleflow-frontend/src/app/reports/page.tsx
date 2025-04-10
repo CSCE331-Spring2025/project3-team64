515a088bd9b9850bfa51d15658bcf157d690f09e
Sophia Phu <sophiatiffphu@gmail.com>
2025-04-09 23:32:40 -0500

    added tabs thingies to ur report

--- bubbleflow-frontend/src/app/reports/page.tsx
+++ bubbleflow-frontend/src/app/reports/page.tsx
@@ -2,11 +2,23 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Reports() {
   return (
     <main className="flex flex-col px-16 pb-4">
-      <p>ur mom</p>
+      <Tabs defaultValue="x-report">
+        <TabsList>
+          <TabsTrigger value="x-report">X Report</TabsTrigger>
+          <TabsTrigger value="z-report">Z Report</TabsTrigger>
+          <TabsTrigger value="sales-report">Sales Report</TabsTrigger>
+        </TabsList>
+        <TabsContent value="x-report">
+          sigma
+        </TabsContent>
+        <TabsContent value="z-report">sigma sigma sigma sigma sigmaa sigma sigmasigma sigma</TabsContent>
+        <TabsContent value="sales-report">sigma sigma sigma</TabsContent>
+      </Tabs>
     </main>
   );
 }
