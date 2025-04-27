c0807a97197152980e202fb54c895568abd21380
Sophia Phu <sophiatiffphu@gmail.com>
2025-04-27 00:18:25 -0500

    add inventory edit popup and fixed google translate tweakin

---
+++ bubbleflow-frontend/src/components/inventoryEditCard.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { RiDeleteBin5Line} from "react-icons/ri";
+
+interface InventoryEditCardProps {
+  name: string;
+  amount: string;
+  badgeText: string;
+  itemId: string;
+}
+
+export function InventoryEditCard({
+  name,
+  amount,
+  badgeText,
+  itemId,
+}: InventoryEditCardProps) {
+  return (
+    <div className="flex flex-col gap-1 border border-primary p-4 rounded-xl">
+      <div className="flex justify-between">
+        <p className="font-semibold text-primary">{name}</p>
+        <p className="font-semibold">{amount}</p>
+      </div>
+      <div className="flex gap-2 justify-between items-center">
+        <div className="flex gap-4 items-center">
+          <Badge className="bg-[#f0dece] text-[#6F403A] font-normal px-3 rounded-3xl">
+            {badgeText}
+          </Badge>
+          <p className="text-sm text-gray-500">ID: {itemId}</p>
+        </div>
+        <div className="flex gap-2 justify-end items-center">
+          <div className="bg-primary w-8 h-8 rounded-full flex items-center justify-center hover:bg-muted hover:-translate-y-1 duration-300">
+            <RiDeleteBin5Line className="text-white" size={20} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
