7669f4607f39528baba734d804c5a6a566fabc58
Sophia Phu <sophiatiffphu@gmail.com>
2025-04-27 11:37:50 -0500

    remove delete bin from inventory edit card

--- bubbleflow-frontend/src/components/inventoryUsageCard.tsx
+++ bubbleflow-frontend/src/components/inventoryUsageCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Badge } from "@/components/ui/badge";
-import { RiDeleteBin5Line} from "react-icons/ri";
 
 interface InventoryUsageCardProps {
   name: string;
@@ -28,11 +27,6 @@ export function InventoryUsageCard({
             {badgeText}
           </Badge>
           <p className="text-sm text-gray-500">ID: {itemId}</p>
-        </div>
-        <div className="flex gap-2 justify-end items-center">
-          <div className="bg-primary w-8 h-8 rounded-full flex items-center justify-center hover:bg-muted hover:-translate-y-1 duration-300">
-            <RiDeleteBin5Line className="text-white" size={20} />
-          </div>
         </div>
       </div>
     </div>
